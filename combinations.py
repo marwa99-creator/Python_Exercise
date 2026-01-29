6a5638bc684ade46112b1df74072e7a178fb27cb
marwa99-creator <marwaboudraa199@gmail.com>
2026-01-29 23:52:17 +0100

    With Fact Recursive Function

--- combinations.py
+++ combinations.py
@@ -7,16 +7,29 @@
 #         result = result * (n - i + 1) // i
 #     return result
 
+# def fact(n):
+#     result = 1
+#     for i in range(1, n + 1):
+#         result *= i
+#     return result
+
+
+# def comb(n, k):
+#     if k < 0 or k > n:
+#         return 0
+#     return fact(n) // (fact(k) * fact(n - k))
+
 def fact(n):
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
+    if n <= 1:
+        return 1
+    return n * fact(n - 1)
 
 
 def comb(n, k):
     if k < 0 or k > n:
         return 0
+    if k > n - k:
+        k = n - k
     return fact(n) // (fact(k) * fact(n - k))
 
 print(comb(10,3))
