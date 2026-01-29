3a0a37d8d260c2c013758b10c9d55b81c5cb5dc7
marwa99-creator <marwaboudraa199@gmail.com>
2026-01-29 23:50:53 +0100

    With fact Iterative function

--- combinations.py
+++ combinations.py
@@ -1,10 +1,22 @@
+# def comb(n, k):
+#     if k < 0 or k > n:
+#         return 0
+
+#     result = 1
+#     for i in range(1, k + 1):
+#         result = result * (n - i + 1) // i
+#     return result
+
+def fact(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+
 def comb(n, k):
     if k < 0 or k > n:
         return 0
-
-    result = 1
-    for i in range(1, k + 1):
-        result = result * (n - i + 1) // i
-    return result
+    return fact(n) // (fact(k) * fact(n - k))
 
 print(comb(10,3))
